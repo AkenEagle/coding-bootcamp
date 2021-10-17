41e35cd4d2f786ae08f28f840701ef7543ebfd33
AkenEagle <akeneagle@gmail.com>
2021-10-17 13:48:17 +0100

    Acitivity 1

--- Week-5/day-01/02-Stu_jQuery-Elements/Unsolved/assets/js/script.js
+++ Week-5/day-01/02-Stu_jQuery-Elements/Unsolved/assets/js/script.js
@@ -1,18 +1,15 @@
-// TODO: Select the existing DOM element, <div id="root">, and assign to a new variable
-// Hint: Visit the documentation https://api.jquery.com/
+const root = $("#root");
 
-// TODO: Create a `<p>` element that will store an author's name and assign that created element to a new variable
+let author = $("<p></p>");
 
-// TODO: Add the following text to the newly created `<p>` element: "~ Carol Dweck"
+author.text("~ Carol Dweck");
+author.addClass(`plain`);
 
-// TODO: Add the class `plain` to the author element
+let quote = $("<h1></h1>");
 
-// TODO: Create a new `<h1>` element that will store an author's quote and assign it to new variable
+quote.text("Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning");
+quote.addClass(`fancy`);
 
-// TODO: Add the following quote text to the quote element, "Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."
+quote.append(author);
 
-// TODO: Apply the class `fancy` to the quote element
-
-// TODO: Append the author element to the quote element
-
-// TODO: Append the quote element to the HTML element with an ID of `root` in `index.html`
+root.append(quote);
